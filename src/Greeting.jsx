9cfa4973cbbbc79208934a4cbf48c76c18ff79f1
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-12-01 19:02:31 +0530

    refactor: remove default template and add logo

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,12 @@
+import logo from "/logo.png";
+
 function Greeting() {
-  return <h1>Greetings! I am Xeno.</h1>;
+  return (
+    <>
+      <img src={logo} alt="" class="logo" />
+      <h1>Greetings! I am Xeno.</h1>
+    </>
+  );
 }
 
 export default Greeting;
