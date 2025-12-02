09f5a03383b8dfb64dccaa80ad929a7323756752
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-12-02 23:22:52 +0530

    feat: add circular logo that follows mouse pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Greeting from "./Greeting.jsx";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
   return (
-    <>
-      <Greeting />
-    </>
+    <div id="content" onPointerMove={e => setMousePosition({ x: e.clientX, y: e.clientY })}>
+      <img
+        src="./logo.png"
+        alt=""
+        className="logo"
+        draggable="false"
+        style={{
+          transform: `translate(${mousePosition.x - 50}px, ${mousePosition.y - 50}px)`,
+        }}
+      />
+    </div>
   );
 }
 
